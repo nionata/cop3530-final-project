f3aa960e52a0ec26c3719a399c4e4aa54346e554
Nicholas Ionata <n.ionata129@gmail.com>
2017-10-31 11:05:54 -0400

    Initial commmit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Nicholas Ionata on 10/31/17.
+//
+//
+
+#include "main.hpp"
---
+++ main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  
+//
+//  Created by Nicholas Ionata on 10/31/17.
+//
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
